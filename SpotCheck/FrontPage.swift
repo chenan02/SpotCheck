8a7ad7a6456ed9bf6d634fe45e0428fb47e8814e
Chris Raveendra <craveend@umich.edu>
2016-11-07 00:45:23 -0500

    implementing location services

--- SpotCheck/FrontPage.swift
+++ SpotCheck/FrontPage.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class FrontPage: UIViewController {
+class FrontPage: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var latitude = locations[0].coordinate.latitude
+        var longitude = locations[0].coordinate.longitude
+    }
+    
     @IBAction func searchMap(_ sender: Any) {
     }
     
