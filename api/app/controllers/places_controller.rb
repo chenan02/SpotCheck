5c787b4ca738429f8fb874396cf7b030817894db
Andrew Chen <chenan02@gmail.com>
2016-12-14 13:47:09 -0500

    Add hour labels to graph

--- api/app/controllers/places_controller.rb
+++ api/app/controllers/places_controller.rb
@@ -98,6 +98,7 @@ class PlacesController < ApplicationController
         end
         time = Time.new
         @occupancy_day = OccupancyDay.find_by(place_id: @place.id, day: time.wday)
+        @hours = ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"]
     end
 
     # not used
